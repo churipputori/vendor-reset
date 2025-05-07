94b240683f2780a9f40f4a642493f281018193b7
churipputori <tori@tori-pc.tailab012.ts.net>
2025-05-07 14:28:08 -0400

    Revert "Fix asm build failure"
    This reverts commit 941e91cfec494604920bdaf1dee9cda1e5955f6c.

--- src/amd/amdgpu/atom.c
+++ src/amd/amdgpu/atom.c
@@ -29,7 +29,7 @@
 #include <linux/slab.h>
 #include <linux/delay.h>
 #include <linux/version.h>
-#include <linux/unaligned.h>
+#include <asm/unaligned.h>
 
 //#include <drm/drm_util.h>
 //#include <drm/drm_print.h>
